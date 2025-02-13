9cf41d00fd3b6d478c798275d556a233e5a45db9
Elvis Dukaj <elvis.dukaj@gmail.com>
2025-02-13 20:03:10 +0100

    refactoring

---
+++ experiments/pre_10_vis_mesh/game/game.cpp
@@ -0,0 +1,138 @@
+
+module;
+
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_events.h>
+
+#include <print>
+#include <ratio>
+#include <stdexcept>
+
+export module Game;
+
+import vis;
+
+export {
+    namespace Game {
+
+        constexpr int SCREEN_HEIGHT = 600;
+        constexpr std::ratio<4, 3> ASPECT_RATIO;
+        constexpr int SCREEN_WIDTH = 800; // SCREEN_HEIGHT * ASPECT_RATIO.num / ASPECT_RATIO.den;
+
+        class App {
+        public:
+            static App *create() {
+                static SDL_Window *window = SDL_CreateWindow("Hello OpenGL", SCREEN_WIDTH, SCREEN_HEIGHT, screen_flags);
+
+                if (not window) {
+                    throw std::runtime_error(std::format("Unable to create the window: {}", SDL_GetError()));
+                }
+
+                static auto engine = vis::engine::create(window);
+                return new App{window, engine};
+            }
+
+            ~App() {
+                SDL_DestroyWindow(window);
+            }
+
+            [[nodiscard]] SDL_AppResult processEvent(const SDL_Event *event) noexcept {
+                if (event->type == SDL_EVENT_QUIT) {
+                    return SDL_AppResult::SDL_APP_SUCCESS;
+                }
+
+                switch (event->type) {
+                    case SDL_EVENT_KEY_DOWN: {
+                        switch (event->key.key) {
+                            case SDLK_ESCAPE:
+                                return SDL_AppResult::SDL_APP_SUCCESS;
+                            default:
+                                break;
+                        }
+                    }
+                        break;
+                    case SDL_EVENT_MOUSE_MOTION: {
+
+                    }
+                        break;
+                    case SDL_EVENT_WINDOW_RESIZED:
+                        screen_width = event->window.data1;
+                        screen_height = event->window.data2;
+                        engine.set_viewport(0, 0, screen_width, screen_height);
+                }
+
+                return SDL_AppResult::SDL_APP_CONTINUE;
+            }
+
+            static auto now() -> float {
+                return static_cast<float>(SDL_GetTicks()) / 1000.0f;
+            }
+
+            [[nodiscard]] SDL_AppResult update() noexcept {
+                const auto current_time = now();
+                const auto dt = current_time - time_start;
+                time_start = current_time;
+
+                engine.clear();
+                circle.draw(*program);
+                engine.render(window);
+
+                return SDL_AppResult::SDL_APP_CONTINUE;
+            }
+
+        private:
+            explicit App(SDL_Window *window, vis::engine::Engine &engine)
+                    : window{window}, engine(engine), time_start{App::now()}, program{std::nullopt},
+                      circle{vis::mesh::create_regular_shape(vis::vec2{}, 0.5f, vis::vec4{}, 50)} {
+                initialize_video();
+            }
+
+            void initialize_video() {
+                engine.print_info();
+                engine.set_clear_color(vis::vec4(1.0f, 0.5f, 0.5f, 1.0f));
+                engine.set_viewport(0, 0, screen_width, screen_height);
+
+                program = vis::opengl::ProgramBuilder{}
+                        .add_shader(vis::opengl::Shader::create(vis::opengl::ShaderType::vertex, R"(
+#version 410 core
+layout (location = 0) in vec2 pos;
+
+void main()
+{
+    gl_Position = vec4(pos.x, pos.y, 0.0f, 1.0);
+}
+)"))
+                        .add_shader(vis::opengl::Shader::create(vis::opengl::ShaderType::fragment, R"(
+#version 410 core
+out vec4 FragColor;
+
+void main()
+{
+    FragColor = vec4(0.0f, 0.5f, 0.2f, 1.0f);
+}
+)"))
+                        .build();
+
+                if (not program) {
+                    throw std::runtime_error{"Unable to initialize the video"};
+                }
+            }
+
+        private:
+            SDL_Window *window = nullptr;
+            vis::engine::Engine &engine;
+
+            int screen_width = SCREEN_WIDTH;
+            int screen_height = SCREEN_HEIGHT;
+            static constexpr SDL_WindowFlags screen_flags = SDL_WINDOW_OPENGL;
+
+            const float border_thickness = 10.0f;
+            const float pad_length = 100.0f;
+
+            float time_start;
+            std::optional<vis::opengl::Program> program{};
+            vis::mesh::Mesh circle;
+        };
+
+    } // namespace Game
+}
---
+++ experiments/pre_10_vis_mesh/vis/lib/vis.cpp
@@ -0,0 +1,6 @@
+export module vis;
+
+export import :math;
+export import :engine;
+export import :opengl;
+export import :mesh;
